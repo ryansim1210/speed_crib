c5bc9c9f433a95666c49577dcb2fac8f2ff98406
ryansim1210 <ryansimpson12103@gmail.com>
2024-10-16 21:34:03 -0400

    added cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactComponent as SvgCards } from './svg-cards.svg';
+
+import Draggable from 'react-draggable'; // Import the Draggable component
+
+
 
 function App() {
+
+  const handleCardClick = () => {
+    alert('Card clicked!');
+  };
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} alt="ace-of-spades" /> */}
+        <Draggable>
+          <div onClick={handleCardClick}
+            style={{
+              width: '100px',
+              height: '150px',
+              cursor: 'pointer'
+            }}> {/* Just for debugging */}
+            <svg width="100" height="150" viewBox="0 0 170 230">
+              <SvgCards />
+              <use xlinkHref="#spade_1" />
+            </svg>
+          </div>
+        </Draggable>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Speed Crib
         </p>
         <a
           className="App-link"
@@ -16,7 +40,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> 
       </header>
     </div>
   );
